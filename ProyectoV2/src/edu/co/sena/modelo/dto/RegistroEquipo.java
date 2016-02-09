43c99d842ad0638fe24e4a9c899e99c058c27bff
Manzon772@gmail.com <>
2016-02-09 13:59:27 -0500

    Se volvio a crear esa mierda -.-

---
+++ ProyectoV2/src/edu/co/sena/modelo/dto/RegistroEquipo.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.co.sena.modelo.dto;
+
+import java.sql.Date;
+
+/**
+ *
+ * @author ADSI - 901540
+ */
+public class RegistroEquipo {
+
+    Date fechaEntrada;
+}
